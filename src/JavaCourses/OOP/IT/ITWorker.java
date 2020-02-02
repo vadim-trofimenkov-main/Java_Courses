8e9abea6c43415b0c4593ec0d59236ec01220694
vadim-trofimenkov-main <55622547+vadim-trofimenkov-main@users.noreply.github.com>
2020-02-02 22:25:35 +0300

    Delete ITWorker.java

--- src/JavaCourses/OOP/IT/ITWorker.java
+++
@@ -1,9 +0,0 @@
-package JavaCourses.OOP.IT;
-
-public class ITWorker implements ICoffeeMaker {
-
-    @Override
-    public void makeCoffee() {
-        System.out.println("Вот ваш Кофе, сэр");
-    }
-}
