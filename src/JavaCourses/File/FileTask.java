2074114658b804c801c48daff03bfd2813e2eeb4
vadim <vadimtrofimenkov97@gmail.com>
2020-03-08 12:21:12 +0300

    File + Reflection API

---
+++ src/JavaCourses/File/FileTask.java
@@ -0,0 +1,32 @@
+package JavaCourses.File;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class FileTask {
+    public static void main(String[] args) {
+        textToConsole(new File("copyTestFile.txt"));
+
+    }
+
+    private static void insertText() {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        try (FileWriter writer = new FileWriter("copyTestFile.txt")) {
+            writer.write(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void textToConsole(File file) {
+        try (Scanner scanner = new Scanner(file); FileReader fileReader = new FileReader(file); FileWriter writer = new FileWriter("copyTestFile.txt")) {
+            char[] text = new char[128];
+            while (fileReader.read(text) > 0) ;
+            writer.write(text);
+            System.out.println(text);
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+    }
+}
---
+++ src/JavaCourses/ReflectionAPI/Person.java
@@ -0,0 +1,24 @@
+package JavaCourses.ReflectionAPI;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person() {
+        name = "Noone";
+        age = 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
